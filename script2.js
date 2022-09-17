5b3c6e49ba97db6a9e630e68fefc300ce2dc552c
SebastianMO17 <113805607+SebastianMO17@users.noreply.github.com>
2022-09-17 17:34:16 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+*{
+const colors=["red","Green","blue","yellow","pink", "purple","orange","black", "white", "gray", "brown",
+"violet", "indigo", "gold", "silver", "cyan", "magenta", "lime","maroon", "navy", "olive", "teal", "aqua", "fuchsia",
+"crimson", "coral", "azure", "beige","cornflowerblue", "darkkhaki"];
+
+const btnsz
+}
